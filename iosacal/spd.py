206fdb93024b4ffc22eb6b4f56128b2f2f6e38d3
Timur Timerkhanov <timurgrunge@gmail.com>
2019-06-20 12:44:33 +0300

    Add numpy import

--- iosacal/spd.py
+++ iosacal/spd.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with IOSACal.  If not, see <http://www.gnu.org/licenses/>.
 
+import numpy as np
 
 def spdsum(spdlist, norm=True):
     """ Sums several SPDs stored in a list.
